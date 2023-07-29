ee5e9794096dd1f958d9ee1448bfd01572ec9a42
echeng8 <dr.frolicks@gmail.com>
2023-07-29 15:36:27 -0700

    add 2 windows to DemoChatGPT

--- Examples/StereoKitTest/Demos/DemoChatGPT.cs
+++ Examples/StereoKitTest/Demos/DemoChatGPT.cs
@@ -1,14 +1,65 @@
 ﻿using StereoKit;
+using System;
+
+class WindowInfo
+{
+    public Pose WindowPose;
+    public string WindowName { get; private set; }
+    public string WindowText { get; private set; }
+
+    public WindowInfo(string name, Pose initialPose, string initialText)
+    {
+        WindowName = name;
+        WindowPose = initialPose;
+        WindowText = initialText;
+    }
+
+    public void UpdateWindowText()
+    {
+        WindowText = $"Updated text for {WindowName} at {DateTime.Now}";
+    }
+
+    public void Show()
+    {
+        UI.WindowBegin(WindowName, ref WindowPose);
+        UI.Label(WindowText);
+
+        if (UI.Button("Update Text"))
+        {
+            UpdateWindowText();
+        }
+
+        UI.WindowEnd();
+    }
+}
 
 class DemoChatGPT : ITest
 {
+    private WindowInfo window1;
+    private WindowInfo window2;
 
+    public void Initialize()
+    {
+        window1 = new WindowInfo("Window 1", new Pose(0, 0, 0, Quat.Identity), "Initial Text for Window 1");
+        window2 = new WindowInfo("Window 2", new Pose(1f, 0, 0, Quat.Identity), "Initial Text for Window 2");
+    }
 
-	public void Update()
-	{
-		
-	}
+    public void Update()
+    {
+        window1.Show();
+        window2.Show();
+    }
 
-	public void Initialize() { }
-	public void Shutdown() { }
+    public void Shutdown() { }
+
+    // Methods to retrieve window text, if needed
+    public string GetWindowText1()
+    {
+        return window1.WindowText;
+    }
+
+    public string GetWindowText2()
+    {
+        return window2.WindowText;
+    }
 }
