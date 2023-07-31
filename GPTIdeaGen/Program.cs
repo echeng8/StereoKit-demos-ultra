c88757de0bbf9ebe54ff3fe888f161ad1ae8be36
echeng8 <dr.frolicks@gmail.com>
2023-07-30 21:10:51 -0700

    working ideagen hellowrold

--- GPTIdeaGen/Program.cs
+++ GPTIdeaGen/Program.cs
@@ -1,19 +1,26 @@
 ﻿using ChatGPT.Net;
+using ChatGPT.Net.DTO.ChatGPT;
 using System.IO;
 
 var apiKey = Environment.GetEnvironmentVariable("CHATGPT_API_KEY", EnvironmentVariableTarget.User);
+// @TODO upgrade to gpt 4 api  
 var bot = new ChatGpt(apiKey);
 
 var prompt = string.Empty;
 
 while (true)
-{     
+{
+    string systemPrompt = File.ReadAllText("SystemPrompt.txt");
+    ChatGptConversation conversation = bot.GetConversation(null);
+    bot.SetConversationSystemMessage(conversation.Id, systemPrompt); 
+
     Console.Write("You: ");
     prompt = Console.ReadLine();
     if (prompt is null) break;
     if (string.IsNullOrWhiteSpace(prompt)) break;
     if (prompt == "exit") break;
     Console.Write("ChatGPT: ");
-    await bot.AskStream(Console.Write, prompt, "default");
-    Console.WriteLine();
+    string resp = await bot.Ask(prompt, conversation.Id);
+    Console.WriteLine(resp);
+
 }
--- test.ps1
+++ test.ps1
@@ -1,27 +1,4 @@
 # Set your API key
 $apiKey = "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"
 
-# Endpoint URL
-$url = "https://api.openai.com/v1/chat/completions"
-
-# Headers
-$headers = @{
-    "Content-Type" = "application/json"
-    "Authorization" = "Bearer $apiKey"
-}
-
-# Body data
-$body = @{
-    model = "gpt-3.5-turbo"
-    messages = @(@{role="user"; content="Say this is a test!"})
-    temperature = 0.7
-}
-
-# Convert body data to JSON format
-$bodyJson = $body | ConvertTo-Json
-
-# Make the request
-$response = Invoke-RestMethod -Uri $url -Headers $headers -Method Post -Body $bodyJson
-
-# Print the response
-$response
+curl https://api.openai.com/v1/models -H "Authorization: Bearer $apiKey"
